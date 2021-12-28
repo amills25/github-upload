ea310fc4e3e541fdab104d257cbfa9c6bfc0e322
amills25 <awmills25@gmail.com>
2021-12-28 13:39:26 -0500

    setting up

---
+++ hello/code.py
@@ -0,0 +1,22 @@
+msg = "Hello World"
+print(msg)
+if 1 < 2:
+    # print('perform code')
+    print('cheers mate')
+"""
+this is a comment
+i do what i want
+"""
+
+x = 5
+y = 1
+print(x)
+print(y)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+print("Hello")
